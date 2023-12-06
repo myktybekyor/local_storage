d30cffe6a77a2f49da7503a89c5ed511f6e7a059
myktybekyor <myktybekme28@mail.ru>
2023-12-06 10:36:38 +0600

    first commit

---
+++ components/Header/Header.js
@@ -0,0 +1,22 @@
+class Header {
+    handlerOpenShoppingPage() {
+        shoppingPage.render();
+    }
+
+    render(count) {
+        const html = `
+            <div class="header-container">
+                <div class="header-counter" onclick="headerPage.handlerOpenShoppingPage();">
+                    🎁 ${count}
+                </div>
+            </div>
+        `;
+
+        ROOT_HEADER.innerHTML = html;
+    }
+}
+
+const headerPage = new Header();
+
+const productsStore = localStorageUtil.getProducts();
+headerPage.render(productsStore.length);
---
+++ constants/catalog.js
@@ -0,0 +1,62 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'TIRO 21 TRACK PANTS',
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/ddb5531498554662a63aac310104b571_9366/Tiro_21_Track_Pants_Black_GH7305_01_laydown.jpg',
+        price: 23,
+    },
+    {
+        id: 'el2',
+        name: 'NO-SHOW SOCKS 6 PAIRS',
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/be851cd9b62947929a68a89e0154d1af_9366/No-Show_Socks_6_Pairs_White_101642_03_standard.jpg',
+        price: 16,
+    },
+    {
+        id: 'el3',
+        name: 'AEROREADY DESIGNED TO MOVE WOVEN SPORT SHORTS',
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/530cd80f75104674bdf2acc60111e0a5_9366/AEROREADY_Designed_to_Move_Woven_Sport_Shorts_Blue_GT8162_01_laydown.jpg',
+        price: 30,
+    },
+    {
+        id: 'el4',
+        name: 'AEROREADY DESIGNED TO MOVE FEELREADY SPORT TEE',
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/25c1191c68a84899b84bac850000a357_9366/AEROREADY_Designed_to_Move_Feelready_Sport_Tee_Grey_GT5554_01_laydown.jpg',
+        price: 25,
+    },
+    {
+        id: 'el5',
+        name: 'ULTRABOOST DNA CITY XPLORER OUTDOOR TRAIL SHOES',
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/113ed7bfedf242489d79aed7016d10b6_9366/Ultraboost_DNA_City_Xplorer_Outdoor_Trail_Shoes_Green_GV8697_01_standard.jpg',
+        price: 100,
+    },
+    {
+        id: 'el6',
+        name: 'TIRO 23 PRO SHORT SLEEVE GRAPHIC JERSEY',
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/059ec95485d94e10b828aeec00bafb13_9366/Tiro_23_Pro_Short_Sleeve_Graphic_Jersey_Black_HN7015_01_laydown.jpg',
+        price: 18,
+    },
+    {
+        id: 'el7',
+        name: 'CAMO SERIES ALLOVER PRINT HOODIE',
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/eb500e282cae4da79f3dae7001000055_9366/Camo_Series_Allover_Print_Hoodie_Yellow_HK2804_01_laydown.jpg',
+        price: 51,
+    },
+    {
+        id: 'el8',
+        name: 'STADIUM FLEECE BADGE OF SPORT HOODIE',
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/0a9a080e4b4340f29828aee500ba6c63_9366/Stadium_Fleece_Badge_of_Sport_Hoodie_Purple_HL1601_01_laydown.jpg',
+        price: 60,
+    },
+    {
+        id: 'el9',
+        name: 'TIRO TRACK PANTS',
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/6c41cc58ba9c42c3bb13aca70119fd02_9366/Tiro_Track_Pants_Red_GT6841_01_laydown.jpg',
+        price: 25,
+    },
+    {
+        id: 'el10',
+        name: 'ARGENTINA GAME DAY TRAVEL PANTS',
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/f4f43c719638439ea406aef500c644b5_9366/Argentina_Game_Day_Travel_Pants_White_IC4448_01_laydown.jpg',
+        price: 95,
+    },
+];
